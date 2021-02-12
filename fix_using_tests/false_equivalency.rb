b5ec5fce2d8a59a5a8d17be1ca61dbf4741df16d
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-12 20:29:55 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 def get_user_input
   gets.chomp
 end
@@ -10,10 +10,10 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 2
-    "HAM HAM HAM IN MY TUMMY"
-  elsif num = 1
-   "YUM YUM MUNCH MUNCH MUNCH"
+  if num = 1
+    "YUM YUM MUNCH MUNCH MUNCH"
+  elsif num = 2
+   "HAM HAM HAM IN MY TUMMY"
   end
   selection("1")
 
