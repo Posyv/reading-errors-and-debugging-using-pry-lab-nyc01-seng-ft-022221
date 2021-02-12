da54d3953ef5ac9186d71eca9886ac0ebe4a2771
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-12 21:02:10 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -5,12 +5,14 @@
 #end
 
 
-def state_log(star_date)
-  state_log = (rand(100000) + 400000) / 10.0
-  "Captain's Log, star date #{state_log}."
-end
+#def state_log(star_date)
+#  engage = (rand(100000) + 400000) / 10.0
+#  "Captain's Log, star date #{state_log}."
+#end
 
-def engage
-  puts state_log(date)
-  date = generate_star_date
+def engage (star_date)
+  star_date = (rand(100000) + 400000) / 10.0
+  "Captain's Log, star date #{star_date}."
+  puts engage 
+
 end
