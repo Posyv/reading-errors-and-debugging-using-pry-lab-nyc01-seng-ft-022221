e4c12cca99a1a5b1f7d15992a05f188984b1cb8b
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-12 21:56:08 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -2,15 +2,15 @@ require 'pry'
 
 def generate_star_date
   binding.pry
-(rand(100000) + 400000) / 10.0
+#(rand(100000) + 400000) / 10.0
 end
 
 
-def state_log(star_date)
- "Captain's Log, star date #{star_date}."
-end
-
-def engage
-engage_1 = generate_star_date
-#state_log
-end
+#def state_log(star_date)
+ #{}"Captain's Log, star date #{star_date}."
+# end
+# 
+# def engage
+# engage_1 = generate_star_date
+# #state_log
+# end
