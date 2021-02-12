6ef01c69ef98fe5da673b2787ada59ec16aaca8a
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-12 20:37:04 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,13 +1,13 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  player1 = 'Beyonce'
-  player2 = 'Jay-z'
+
   "Hello #{player1} & #{player2}"
 end
+start_game('Beyonce', 'Jay-z')
 
 def play_game(player1, player2)
-  player1 = 'Beyonce'
-  player2 = 'Jay-z'
+
   "#{player1} is better than #{player2}"
 end
+play_game('Beyonce', 'Jay-z')
