3e4306165a76c0e899bf84bb2af418b6098294a1
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-12 21:50:54 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,14 +1,15 @@
-# don't forget to add: require 'pry'
-
+require 'pry'
 def generate_star_date
 (rand(100000) + 400000) / 10.0
 end
 
 
 def state_log(star_date)
+  binding.pry
  "Captain's Log, star date #{star_date}."
 end
 
 def engage
- 
+engage_1 = generate_star_date 
+state_log 
 end
